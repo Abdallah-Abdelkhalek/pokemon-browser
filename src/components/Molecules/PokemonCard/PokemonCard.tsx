3ce59acf45eb658e0d0845c079ab3ba15a730ca8
Abdallah Abdelkhalek <abdallah.abdelkhalek98@gmail.com>
2025-10-25 22:19:25 +0300

    Refactor PokemonCard component to use internal image source and add PokemonCardSkeleton for loading state

--- src/components/Molecules/PokemonCard/PokemonCard.tsx
+++ src/components/Molecules/PokemonCard/PokemonCard.tsx
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom";
 
 interface Props {
   name: string;
-  image: string;
   id: number;
 }
 
-export const PokemonCard: React.FC<Props> = ({ name, image, id }) => (
+export const PokemonCard: React.FC<Props> = ({ name, id }) => (
   <Link
     to={`/pokemon/${name}`}
     className="bg-white rounded-xl shadow hover:scale-105 transition p-4 flex flex-col items-center"
   >
     <img
-      src={image}
+      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+      // fallback to dream-world svg if official artwork is missing
+      onError={(e) =>
+        (e.currentTarget.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`)
+      }
       alt={name}
       className="w-full h-full aspect-square bg-[#f6f7f9] rounded-lg"
+      loading="lazy"
     />
     <h3 className="capitalize mt-2 font-semibold">{name}</h3>
     <span>#{id.toString().padStart(3, "0")}</span>
