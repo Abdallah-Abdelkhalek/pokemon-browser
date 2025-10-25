f7ef0ac57b48cb754e0eecfae8cea8bb247a116c
Abdallah Abdelkhalek <abdallah.abdelkhalek98@gmail.com>
2025-10-26 00:35:52 +0300

    Fix skeleton card UI

--- src/components/Molecules/PokemonCard/PokemonCardSkeleton.tsx
+++ src/components/Molecules/PokemonCard/PokemonCardSkeleton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const PokemonCardSkeleton: React.FC = () => (
-  <div className="bg-white rounded-xl shadow p-4 flex flex-col items-center animate-pulse">
+  <div className="bg-white rounded-xl shadow p-4 flex flex-col items-start animate-pulse">
     <div className="w-full aspect-square bg-gray-200 rounded-lg" />
     <div className="w-3/4 h-4 bg-gray-200 rounded mt-3" />
     <div className="w-1/2 h-3 bg-gray-200 rounded mt-2" />
