155c65088b9235ab107e80bd6e27ca7208c4407c
Abdallah Abdelkhalek <abdallah.abdelkhalek98@gmail.com>
2025-10-26 15:42:23 +0300

    Add showBall prop to Spinner component for displaying a Pokeball image during loading

--- src/components/Atoms/Spinner/Spinner.tsx
+++ src/components/Atoms/Spinner/Spinner.tsx
@@ -1,18 +1,28 @@
 interface SpinnerProps {
   color?: string;
+  showBall?: boolean;
 }
+import Pokeball from "../../../Assets/pngs/pokemon-ball.png";
 
-export const Spinner = ({ color }: SpinnerProps) => {
+export const Spinner = ({ color, showBall = false }: SpinnerProps) => {
   const spinnerColor = color || "#3b82f6";
-
   return (
     <div className="flex justify-center items-center h-10">
-      <div
-        className="w-10 h-10 rounded-full border-4 border-t-transparent animate-spin"
-        style={{
-          borderColor: `${spinnerColor} transparent ${spinnerColor} ${spinnerColor}`,
-        }}
-      ></div>
+      {showBall ? (
+        <img
+          src={Pokeball}
+          alt="Loading..."
+          className="animate-spin w-16 h-16 mb-4"
+          style={{ animationDuration: "1s" }}
+        />
+      ) : (
+        <div
+          className="w-10 h-10 rounded-full border-4 border-t-transparent animate-spin"
+          style={{
+            borderColor: `${spinnerColor} transparent ${spinnerColor} ${spinnerColor}`,
+          }}
+        ></div>
+      )}
     </div>
   );
 };
