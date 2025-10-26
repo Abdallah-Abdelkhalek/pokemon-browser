dc182c40fa7d1278f021dc25b288fc2a66af393c
Abdallah Abdelkhalek <abdallah.abdelkhalek98@gmail.com>
2025-10-26 16:33:52 +0300

    Refactor ErrorFallback component to include a homepage link and improve error handling UI;

--- src/components/Molecules/ErrorFallback/ErrorFallback.tsx
+++ src/components/Molecules/ErrorFallback/ErrorFallback.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 interface ErrorFallbackProps {
   error: Error;
   resetErrorBoundary: () => void;
@@ -21,11 +20,19 @@ export const ErrorFallback: React.FC<ErrorFallbackProps> = ({
     <p className="text-gray-700 dark:text-gray-300 mb-4 max-w-md">
       {error.message || "We couldn’t catch that Pokémon!"}
     </p>
-    <button
-      onClick={resetErrorBoundary}
-      className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg shadow"
-    >
-      Try Again
-    </button>
+    <div className="flex items-center gap-4">
+      <a
+        href="/"
+        className="px-4 py-2 rounded-lg border border-red-300/50 bg-white/20 backdrop-blur-sm text-white font-medium shadow-sm transition-all duration-200 hover:bg-white/40 hover:shadow-md "
+      >
+        Homepage
+      </a>
+      <button
+        onClick={resetErrorBoundary}
+        className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg shadow"
+      >
+        Try Again
+      </button>
+    </div>
   </div>
 );
